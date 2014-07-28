f31afd090c0445b3badbbd1d0401ef464fe3275b
Virgilio N Santos <virgilio.santos@gmail.com>
2014-07-28 19:21:58 -0300

    justification: add simple template and producer

---
+++ course-tool/src/java/br/unicamp/iel/tool/producers/JustificationProducer.java
@@ -0,0 +1,47 @@
+package br.unicamp.iel.tool.producers;
+
+import lombok.Setter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import uk.org.ponder.rsf.components.UIContainer;
+import uk.org.ponder.rsf.components.UIInternalLink;
+import uk.org.ponder.rsf.view.ComponentChecker;
+import uk.org.ponder.rsf.view.ViewComponentProducer;
+import uk.org.ponder.rsf.viewstate.SimpleViewParameters;
+import uk.org.ponder.rsf.viewstate.ViewParameters;
+import br.unicamp.iel.logic.ReadInWebCourseLogic;
+
+/**
+ *
+ * @author Andre Zanchetta
+ * @author Virgilio Santos
+ *
+ */
+public class JustificationProducer implements ViewComponentProducer {
+
+    public static final String VIEW_ID = "justification";
+
+    private static Log logger = LogFactory.getLog(JustificationProducer.class);
+
+    @Setter
+    private ReadInWebCourseLogic logic;
+
+    @Override
+    public String getViewID() {
+        return VIEW_ID;
+    }
+
+
+    @Override
+    public void fillComponents(UIContainer tofill, ViewParameters viewparams,
+            ComponentChecker checker) {
+
+        System.out.println("Justificativa");
+        UIInternalLink.make(tofill, "link_home",
+                new SimpleViewParameters(SummaryProducer.VIEW_ID));
+        UIInternalLink.make(tofill, "link_justification", viewparams);
+
+    }
+}
