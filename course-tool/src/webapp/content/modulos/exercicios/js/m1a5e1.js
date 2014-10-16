79ef412a033320e9f16598ac6a6a2775ec70a4a3
Ana Rute <anarutecvm@gmail.com>
2014-10-16 21:06:17 +0100

    m1a5e1: align answer with inputs

--- course-tool/src/webapp/content/modulos/exercicios/js/m1a5e1.js
+++ course-tool/src/webapp/content/modulos/exercicios/js/m1a5e1.js
@@ -1,27 +1,26 @@
 
   $(document).ready(function(){
     $(".respos").click(function(){
-    
+
         var certo = "/readinweb-course-tool/content/modulos/exercicios/imagens/certo.gif";
         var errado = "/readinweb-course-tool/content/modulos/exercicios/imagens/errado.gif";
 
-        if ($(this).prev().prev().children('input').val().toUpperCase() == 
+        if ($(this).prev().prev().children('input').val().toUpperCase() ==
         $(this).next().children('.resposta1').text().toUpperCase() ){
         	$(this).prev().prev().children('img').attr('src', certo);
         }
 		else {
-			$(this).next().children('.resposta1').show('slow');
+			$(this).next().children('.resposta1').css('visibility','visible');
 			$(this).prev().prev().children('img').attr('src', errado);
 		}
-		if ($(this).prev().children('input').val().toUpperCase() == 
+		if ($(this).prev().children('input').val().toUpperCase() ==
 		$(this).next().children('.resposta2').text().toUpperCase()){
 			$(this).prev().children('img').attr('src', certo);
 		}
 		else {
-			$(this).next().children('.resposta2').show('slow');
+			$(this).next().children('.resposta2').css('visibility','visible');
 			$(this).prev().children('img').attr('src', errado);
 		}
 	 });
- 
+
   });
- 
