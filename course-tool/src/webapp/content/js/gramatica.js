e9677cc482a5f294bacc7d581bbdb09fd4d0aa7e
Virgilio N Santos <virgilio.santos@gmail.com>
2014-12-01 09:18:14 -0200

    grammar: fix/hack firefox iframe issue

--- course-tool/src/webapp/content/js/gramatica.js
+++ course-tool/src/webapp/content/js/gramatica.js
@@ -10,5 +10,16 @@ var indice_affix = function() {
 };
 
 $(document).ready( function() {
-	indice_affix;
-});
+        indice_affix;
+        $("a[href='#topo']").click(function(){
+                $("html,body", window.parent.document).scrollTop(0);
+                return false;
+                });
+        $("#lista_indice_gramatica ul li a").click(function(){
+                console.log("oi oi");
+                /*$("html, body", window.parent.document).animate({
+                 *                         'scrollTop':   $($(this).attr("href")).offset().top
+                 *                                         }, 2000);*/
+                $("html, body", window.parent.document).scrollTop($($(this).attr("href")).offset().top);
+                });
+        });
