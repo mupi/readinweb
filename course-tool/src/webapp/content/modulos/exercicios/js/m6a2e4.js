287e556ffa7fdf0991312a902e3d61f4ba0d837c
Ana Rute <anarutecvm@gmail.com>
2014-12-15 22:20:32 -0200

    m6a2e4: fix answer

--- course-tool/src/webapp/content/modulos/exercicios/js/m6a2e4.js
+++ course-tool/src/webapp/content/modulos/exercicios/js/m6a2e4.js
@@ -3,27 +3,27 @@
     $("#resposta").click(function(){
       var certo = "/readinweb-course-tool/content/modulos/exercicios/imagens/certo.gif";
       var errado = "/readinweb-course-tool/content/modulos/exercicios/imagens/errado.gif";
-        
-      if ($("#opcao1").val() == "1") {
+
+      if ($("#opcao1").val() == "4") {
 	  $("#resp1").attr({ src: certo});
       }
       else {$("#resp1").attr({ src: errado});};
 
-      if ($("#opcao2").val() == "4") {
+      if ($("#opcao2").val() == "1") {
           $("#resp2").attr({ src: certo});
       }
       else {$("#resp2").attr({ src: errado});};
 
-      if ($("#opcao3").val() == "3") {
+      if ($("#opcao3").val() == "2") {
           $("#resp3").attr({ src: certo});
       }
       else {$("#resp3").attr({ src: errado});};
 
-      if ($("#opcao4").val() == "2") {
+      if ($("#opcao4").val() == "3") {
           $("#resp4").attr({ src: certo});
       }
       else {$("#resp4").attr({ src: errado});};
 
     });
- 
+
   });
