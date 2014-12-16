5b963034fcca56bd3ba824437b85bb60246289c5
Ana Rute <anarutecvm@gmail.com>
2014-12-16 10:51:38 -0200

    m6a3e4: fix js and images

--- course-tool/src/webapp/content/modulos/exercicios/js/m6a3e4.js
+++ course-tool/src/webapp/content/modulos/exercicios/js/m6a3e4.js
@@ -1,8 +1,8 @@
-   $(document).ready(function(){
-        
+   $j(document).ready(function(){
+
         //malsup.com/jquery/corner
-        
-        
+
+
 		    //true se clicou no lado esquerdo, false se clicou no lado direito
 		    var clicou_esquerda = false;
 		    var positon_esquerda;
@@ -18,37 +18,37 @@
 		    var certo2 = false;
 		    var certo3 = false;
 		    var certo4 = false;
-		    
+
 		    function desenha_linha( x1, y1, x2, y2, cor, grossura){
-		        $("#linha").drawLine(x1, y1, x2, y2, {color: cor, stroke: grossura});
+		        $j("#linha").drawLine(x1, y1, x2, y2, {color: cor, stroke: grossura});
 		    }
-		    $("input:button").removeAttr("disabled");
+		    $j("input:button").removeAttr("disabled");
 
-		    $(".quadro").mouseover(function() {
-			    $(this).addClass("mouse_over");
+		    $j(".quadro").mouseover(function() {
+			    $j(this).addClass("mouse_over");
 		    });
 
-		    $(".quadro").mouseout(function() {
-			    $(this).removeClass("mouse_over");
+		    $j(".quadro").mouseout(function() {
+			    $j(this).removeClass("mouse_over");
 		    });
 
-		    $(".quadro").click(function() {
-		     	if (($(this).parent("div").attr("id") == "col_esquerda") && !$(this).hasClass("quadro_respondido"))
+		    $j(".quadro").click(function() {
+		     	if (($j(this).parent("div").attr("id") == "col_esquerda") && !$j(this).hasClass("quadro_respondido"))
 			    {
-			    	$(".quadro_clicado").removeClass("quadro_clicado");
-				    $(this).addClass("quadro_clicado");
+			    	$j(".quadro_clicado").removeClass("quadro_clicado");
+				    $j(this).addClass("quadro_clicado");
 		            clicou_esquerda = true;
-		            position_esquerda = $(this).position();
-		            quadro_esquerda = $(this).attr("id");
+		            position_esquerda = $j(this).position();
+		            quadro_esquerda = $j(this).attr("id");
 			    };
-		        if (($(this).parent("div").attr("id") == "col_direita") && (clicou_esquerda) && !$(this).hasClass("quadro_respondido"))
+		        if (($j(this).parent("div").attr("id") == "col_direita") && (clicou_esquerda) && !$j(this).hasClass("quadro_respondido"))
 			    {
 				    if (cont_cliques < 4)
 				    {
-						$(this).addClass("quadro_clicado");
+						$j(this).addClass("quadro_clicado");
 				        clicou_esquerda = false;
-				        position_direita = $(this).position();
-				        quadro_direita = $(this).attr("id");
+				        position_direita = $j(this).position();
+				        quadro_direita = $j(this).attr("id");
 				        x2 = position_direita.left;
 						y2 = position_direita.top + 55; //deixa no meio do quadrinho
 						y1 = position_esquerda.top + 55; //deixa no meio do quadrinho
@@ -58,7 +58,7 @@
 						if ((quadro_esquerda == "esq1") && (quadro_direita == "dir4"))
 						{
 							certo1 = true;
-						} 
+						}
 						else if ((quadro_esquerda == "esq2") && (quadro_direita == "dir3"))
 						{
 							certo2 = true;
@@ -71,18 +71,18 @@
 						{
 							certo4 = true;
 						};
-						
-						$(".quadro_clicado").addClass("quadro_respondido").removeClass("quadro_clicado");
+
+						$j(".quadro_clicado").addClass("quadro_respondido").removeClass("quadro_clicado");
 					}
 			    };
 		    });
-		   
-		   	$("#apagar").click(function(){
-		   		$("#linha_resp").empty();
-		   		$("#linha").empty();
-		   		$(".quadro_respondido").removeClass("quadro_respondido");
-		   		$(".quadro_clicado").removeClass("quadro_clicado");
-		   		
+
+		   	$j("#apagar").click(function(){
+		   		$j("#linha_resp").empty();
+		   		$j("#linha").empty();
+		   		$j(".quadro_respondido").removeClass("quadro_respondido");
+		   		$j(".quadro_clicado").removeClass("quadro_clicado");
+
 		   	   	clicou_esquerda = false;
 				cont_cliques = 0;
 				quadro_esquerda;
@@ -90,64 +90,64 @@
 				certo1 = false;
 				certo2 = false;
 				certo3 = false;
-				certo4 = false;	
-		   		
+				certo4 = false;
+
 		   	});
-		   
-		    $("#resposta").click(function(){
-			    
+
+		    $j("#resposta").click(function(){
+
 		    	var certo = "/readinweb-course-tool/content/modulos/exercicios/imagens/certo.gif";
 		    	var errado = "/readinweb-course-tool/content/modulos/exercicios/imagens/errado.gif";
 			    // ======================== FAZER MOSTRAR FIGURAS DE CERTO OU ERRADO ================================
-			    	
+
 			    if (certo1)
 			    {
 			    	var cor = "green";
-			    	 $('#resp1').attr('src', certo);
+			    	 $j('#resp1').attr('src', certo);
 			    }
 			    else
 			    {
 			    	var cor = "red";
-			    	 $('#resp1').attr('src', errado);
+			    	 $j('#resp1').attr('src', errado);
 			    };
-				//$("#linha_resp").drawLine(x1, y1, x2, y2, {color: cor, stroke: 2});
-			
+				//$j("#linha_resp").drawLine(x1, y1, x2, y2, {color: cor, stroke: 2});
+
 			    if (certo2)
 			    {
 			    	var cor = 'green';
-			    	 $('#resp2').attr('src', certo);
+			    	 $j('#resp2').attr('src', certo);
 			    }
 			    else
 			    {
 			    	var cor = 'red';
-			    	 $('#resp2').attr('src', errado);
+			    	 $j('#resp2').attr('src', errado);
 			    };
-				//$("#linha_resp").drawLine(x1, y1, x2, y2, {color: cor, stroke: 2});
-								
+				//$j("#linha_resp").drawLine(x1, y1, x2, y2, {color: cor, stroke: 2});
+
 			    if (certo3)
 			    {
 			    	var cor = 'green';
-			    	 $('#resp3').attr('src', certo);
+			    	 $j('#resp3').attr('src', certo);
 			    }
 			    else
 			    {
 			    	var cor = 'red';
-			    	 $('#resp3').attr('src', errado);
+			    	 $j('#resp3').attr('src', errado);
 			    };
-				//$("#linha_resp").drawLine(x1, y1, x2, y2, {color: cor, stroke: 2});
-				
+				//$j("#linha_resp").drawLine(x1, y1, x2, y2, {color: cor, stroke: 2});
+
 			    if (certo4)
 			    {
 			    	var cor = 'green';
-			    	 $('#resp4').attr('src', certo);
+			    	 $j('#resp4').attr('src', certo);
 			    }
 			    else
 			    {
 			    	var cor = 'red';
-			    	 $('#resp4').attr('src', errado);
+			    	 $j('#resp4').attr('src', errado);
 			    };
-				//$("#linha_resp").drawLine(x1, y1, x2, y2, {color: cor, stroke: 2});
-				
+				//$j("#linha_resp").drawLine(x1, y1, x2, y2, {color: cor, stroke: 2});
+
 		    });
-		    
+
         });
