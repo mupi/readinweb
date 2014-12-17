a1b6efbf035a8ff9194517a2c15faaa8d1990c0f
Ana Rute <anarutecvm@gmail.com>
2014-12-17 15:51:30 -0200

    m4a1e5: fix answer

--- course-tool/src/webapp/content/modulos/exercicios/js/m4a1e5.js
+++ course-tool/src/webapp/content/modulos/exercicios/js/m4a1e5.js
@@ -1,25 +1,25 @@
   $(document).ready(function(){
     $("#resposta").click(function(){
-      
+
         var certo = "/readinweb-course-tool/content/modulos/exercicios/imagens/certo.gif";
         var errado = "/readinweb-course-tool/content/modulos/exercicios/imagens/errado.gif";
-      
+
       if ($("#opcao1").val() == "1") {
           document.getElementById("resp1").src = certo;
       }
       else {document.getElementById("resp1").src = errado;}
 
-      if ($("#opcao2").val() == "2") {
+      if ($("#opcao2").val() == "3") {
           document.getElementById("resp2").src = certo;
       }
       else {document.getElementById("resp2").src = errado;}
 
-      if ($("#opcao3").val() == "2") {
+      if ($("#opcao3").val() == "1") {
           document.getElementById("resp3").src = certo;
       }
       else {document.getElementById("resp3").src = errado;}
 
-      if ($("#opcao4").val() == "2") {
+      if ($("#opcao4").val() == "3") {
           document.getElementById("resp4").src = certo;
       }
       else {document.getElementById("resp4").src = errado;}
@@ -44,6 +44,6 @@
       }
       else {document.getElementById("resp8").src = errado;}*/
     });
- 
+
   });
- 
+
